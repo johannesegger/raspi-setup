c797902cc036ef7ce6b3199f14118b703a520953
Johannes Egger <j.egger@posteo.at>
2018-04-18 23:13:04 +0200

    Remove programmatic service config fixes

--- run.sh
+++ run.sh
@@ -12,7 +12,6 @@ echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-fr
 wget -qO - https://linux-packages.resilio.com/resilio-sync/key.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install resilio-sync
-sudo sed -i '/WantedBy=multi-user\.target/c\WantedBy=default.target' /usr/lib/systemd/user/resilio-sync.service
 sudo systemctl enable resilio-sync
 
 #echo "Generating SSL certificate"
